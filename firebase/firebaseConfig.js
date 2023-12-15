5648ad7476427f9564e58d32f42448806e0869ed
9raja1999 <draj29209@gmail.com>
2023-12-14 23:20:18 -0800

    file upload task done for uploading students profile picture

--- firebase/firebaseConfig.js
+++ firebase/firebaseConfig.js
@@ -1,4 +1,6 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js";
+import { getDatabase, set, ref, get, child, onChildAdded, push } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-database.js";
+import { getStorage, ref as Sref, uploadBytesResumable, getDownloadURL } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-storage.js";
 import { 
     getAuth,
     createUserWithEmailAndPassword,
@@ -8,7 +10,7 @@ import {
     GoogleAuthProvider,
     GithubAuthProvider,
     signInWithPopup
-} from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
+} from "https://www.gstatic.com/firebasejs/10.6.0/firebase-auth.js";
 
 
 const firebaseConfig = {
@@ -19,17 +21,21 @@ const firebaseConfig = {
     storageBucket: "chatapp-632ea.appspot.com",
     messagingSenderId: "480343205415",
     appId: "1:480343205415:web:9d65028f1ea3f568115002"
-};
+  };
 
 const app = initializeApp(firebaseConfig);
 
 var auth = getAuth(app)
+var db = getDatabase(app)
+// var storage = getStorage();
 var googleProvider = new GoogleAuthProvider()
 var githubProvider = new GithubAuthProvider()
 
 
 export {
     auth,
+    db,
+    getStorage,
     createUserWithEmailAndPassword,
     signInWithEmailAndPassword,
     sendPasswordResetEmail,
@@ -37,4 +43,12 @@ export {
     googleProvider,
     githubProvider,
     signInWithPopup,
+    set,
+    ref,
+    get,
+    child,
+    Sref,
+    uploadBytesResumable,
+    getDownloadURL,
+    push
 }
